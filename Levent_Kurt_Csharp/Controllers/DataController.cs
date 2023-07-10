b40ba4712a53c300b0ce2540fc6c949005974b4b
lkurt <lkurt@ozkainsaat.com.tr>
2023-07-10 17:09:03 +0300

    Add project files.

---
+++ Levent_Kurt_Csharp/Controllers/DataController.cs
@@ -0,0 +1,25 @@
+﻿using Levent_Kurt_Csharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Levent_Kurt_Csharp.Controllers
+{
+    public class DataController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataController()
+        {
+            _httpClient = new HttpClient();
+        }
+        public async Task <IActionResult> Index()
+        {
+
+
+            var apiResponse = await _httpClient.GetStringAsync("https://api.tmgrup.com.tr/v1/link/352");
+            var data = JsonConvert.DeserializeObject<DataModel>(apiResponse);
+            return View(data);
+        }
+    }
+}
